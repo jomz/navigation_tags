jomz/navigation_tags

commit 57184a5b2468480ce9708209cfe51d1471b4ca25

major refactoring, removing r:if_self, r:if_ancestor_or_self etc as these are in standard_tags nowadays

lib/navigation_tags.rb
@@ -7,11 +7,12 @@ module NavigationTags
   desc %{Render a navigation menu. Walks down the directory tree, expanding the tree up to the current page.
 
     *Usage:*
-    <pre><code><r:nav [id="subnav"] [root=\"/products\"] [include_root=\"true\"] [depth=\"2\"] [expand_all=\"true\"]/></code></pre> 
+    <pre><code><r:nav [id="subnav"] [root=\"/products\"] [append_urls=\"/,/about-us/contact\"] [depth=\"2\"] [expand_all=\"true\"]/></code></pre> 
     *Attributes:*
 
     root: defaults to "/", where to start building the navigation from, you can i.e. use "/products" to build a subnav
-    include_root: defaults to false, set to true to include the root page (i.e. Home)
+    append_urls: urls of pages to add to the end of the navigation ul seperated by a comma
+    prepend_urls: urls of pages to add to the beginning of the navigation ul seperated by a comma
     ids_for_lis: defaults to false, enable this to give each li an id (it's slug prefixed with nav_)
     ids_for_links: defaults to false, enable this to give each link an id (it's slug prefixed with nav_)
 
@@ -22,29 +23,40 @@ module NavigationTags
 
   tag "nav" do |tag|
     if tag.double?
-      root = Page.find_by_url(tag.expand)
+      root = Page.find_by_path(tag.expand)
     else
-      root = Page.find_by_url(root_url = tag.attr.delete('root') || "/")
+      root = Page.find_by_path(root_url = tag.attr.delete('root') || "/")
     end
 
     raise NavTagError, "No page found at \"#{root_url}\" to build navigation from." if root.class_name.eql?('FileNotFoundPage')
 
     depth = tag.attr.delete('depth') || 1
-    ['include_root', 'ids_for_lis', 'ids_for_links', 'expand_all', 'first_set'].each do |prop|
+    ['ids_for_lis', 'ids_for_links', 'expand_all', 'first_set', 'prepend_urls', 'append_urls'].each do |prop|
       eval "@#{prop} = tag.attr.delete('#{prop}') || false"
     end
-
-    if @include_root
-      css_class = [("current" if tag.locals.page == root), "first"].compact
-      @first_set = true
-      tree = %{<li#{" class=\"#{css_class.join(" ")}\""}#{" id=\"" +
-      (root.slug == "/" ? 'home' : root.slug) + "\"" if @ids_for_lis}><a href="#{root.url}"#{" id=\"link_" + (root.slug == "/" ? 'home' : root.slug) + "\"" if @ids_for_links}>#{escape_once(root.breadcrumb)}</a></li>\n}
-    else
-      tree = ""
+    
+    lis = []
+    
+    if @prepend_urls
+      @prepend_urls.split(",").compact.each do |url|
+        page = Page.find_by_path(url)
+        if page.class_name != "FileNotFoundPage"
+          lis << li_for_current_page_vs_navigation_item(tag.locals.page, page)
+        end
+      end
     end
 
     for child in root.children
-      tree << tag.render('sub-nav', {:page => child, :depth => depth.to_i - 1 })
+      lis << tag.render('sub-nav', {:page => child, :depth => depth.to_i - 1, :first_set => @first_set })
+    end
+    
+    if @append_urls
+      @append_urls.split(",").compact.each do |url|
+        page = Page.find_by_path(url)
+        if page.class_name != "FileNotFoundPage"
+          lis << li_for_current_page_vs_navigation_item(tag.locals.page, page)
+        end
+      end
     end
 
     if tag.attr
@@ -55,7 +67,7 @@ module NavigationTags
     end
 
     %{<ul#{tag_options}>
-    #{tree}
+    #{lis.join}
     </ul>}
 
   end
@@ -63,29 +75,21 @@ module NavigationTags
   tag "sub-nav" do |tag|
     current_page = tag.locals.page
     child_page = tag.attr[:page]
-    depth = tag.attr[:depth]
-    return if depth < 0 or child_page.virtual? or !child_page.published? or child_page.class_name.eql? "FileNotFoundPage" or child_page.part("no-map")
-    css_class = [
-      ("current" if current_page == child_page),
-      ("has_children" if child_page.children.size > 0),
-      ("parent_of_current" if current_page.url.starts_with?(child_page.url) and current_page != child_page)
-    ]
-    if !@first_set
-      css_class << 'first'
-      @first_set = true
-    end
-    if !@sub_first_set
-      css_class << 'first'
-      @sub_first_set = true
-    end
-    url = child_page.url
-    r = %{\t<li#{" class=\"#{css_class.compact.join(" ")}\"" unless css_class.compact.empty?}#{" id=\"nav_" + child_page.slug + "\"" if @ids_for_lis}>
-    <a href="#{url}"#{" id=\"link_" + (child_page.slug == "/" ? 'home' : child_page.slug) + "\"" if @ids_for_links}>#{escape_once(child_page.breadcrumb)}</a>}
+    @depth ||= tag.attr.delete(:depth)
+    @first_set ||= tag.attr.delete(:first_set)
+    return if @depth < 0 or child_page.virtual? or !child_page.published? or child_page.class_name.eql? "FileNotFoundPage" or child_page.part("no-map")
+    
+    r = %{<li#{li_attrs_for_current_page_vs_navigation_item(current_page, child_page)}>
+      #{link_for_page(child_page)}\n}
+      # mind the open li
     rr = ""
-    if child_page.children.size > 0 and depth.to_i > 0 and child_page.class_name != 'ArchivePage' and (@expand_all || current_page.url.starts_with?(child_page.url) )
-      @sub_first_set = false
+    if child_page.children.size > 0 and 
+        @depth.to_i > 0 and
+        child_page.class_name != 'ArchivePage' and
+        (@expand_all || current_page.url.starts_with?(child_page.url) )
+      @first_set = false
       child_page.children.each do |child|
-        rr << tag.render('sub-nav', :page => child, :depth => depth.to_i - 1 ) unless child.part("no-map") || !child.published?
+        rr << tag.render('sub-nav', :page => child, :depth => @depth.to_i - 1, :first_set => @first_set ) unless child.part("no-map") || !child.published?
       end
       
       r << "<ul>\n" + rr + "</ul>\n" unless rr.empty?
@@ -93,48 +97,41 @@ module NavigationTags
     r << "</li>\n"
   end
 
-
-  # Inspired by this thread: 
-  # http://www.mail-archive.com/radiant@lists.radiantcms.org/msg03234.html
-  # Author: Marty Haught
-  desc %{
-    Renders the contained element if the current item is an ancestor of the current page or if it is the page itself. 
-  }
-  tag "if_ancestor_or_self" do |tag|
-    Page.benchmark "TAG: if_ancestor_or_self - #{tag.locals.page.url}" do
-      tag.expand if tag.globals.page.url.starts_with?(tag.locals.page.url)
+  def li_attrs_for_current_page_vs_navigation_item current_page, child_page
+    classes = [
+      ("current" if current_page == child_page),
+      ("has_children" if child_page.children.size > 0),
+      ("parent_of_current" if current_page.url.starts_with?(child_page.url) and current_page != child_page)
+    ]
+    if !@first_set
+      classes << "first"
+      @first_set = true
+    end
+    
+    result = ""
+    if classes.any?
+      result = " class=\"#{classes.compact.join(" ")}\""
+    end
+    if @ids_for_lis
+      result << " id=\"nav_" + child_page.slug + "\""
+    end
+    result
+  end
+  
+  def li_for_current_page_vs_navigation_item current_page, child_page
+    "<li#{li_attrs_for_current_page_vs_navigation_item(current_page, child_page)}>#{link_for_page(child_page)}</li>"
+  end
+  
+  def link_for_page page
+    if @ids_for_links
+      "<a href=\"#{page.url}\" id=\"#{("link_" + (page.slug == "/" ? 'home' : page.slug))}\">#{escape_once(page.breadcrumb)}</a>"
+    else
+      "<a href=\"#{page.url}\">#{escape_once(page.breadcrumb)}</a>"
     end
   end
-
-  desc %{
-    Renders the contained element if the current item is also the current page. 
-  }
-  tag "if_self" do |tag|
-    Page.benchmark "TAG: if_self - #{tag.locals.page.url}" do
-      tag.expand if tag.locals.page == tag.globals.page
-    end
+  
+  def id_for_link page
+    ' id="link_' + (child_page.slug == "/" ? 'home' : child_page.slug) + '"' if @ids_for_links
   end
-
-  desc %{    
-    Renders the contained elements only if the current contextual page has children.
-
-    *Usage:*
-    <pre><code><r:if_children>...</r:if_children></code></pre>
-  }
-  tag "if_children" do |tag|
-    Page.benchmark "TAG: if_children - #{tag.locals.page.url}" do
-      tag.expand if tag.locals.page.children.size > 0
-    end
-  end
-
-  tag "unless_children" do |tag|
-    tag.expand unless tag.locals.page.children.size > 0
-  end
-
-  tag "benchmark" do |tag|
-    Page.benchmark "BENCHMARK: #{tag.attr['name']}"do
-    tag.expand
-  end
-end
 
 end

commit a30e78457abe6fdf199e30a9d74ffef53f27e700

fixing depth issue with nested navigation lists (introduced in 57184a5b2468480ce9708209cfe51d1471b4ca25 )

lib/navigation_tags.rb
@@ -77,21 +77,21 @@ module NavigationTags
   tag "sub-nav" do |tag|
     current_page = tag.locals.page
     child_page = tag.attr[:page]
-    @depth ||= tag.attr.delete(:depth)
+    depth = tag.attr.delete(:depth)
     @first_set ||= tag.attr.delete(:first_set)
-    return if @depth < 0 or child_page.virtual? or !child_page.published? or child_page.class_name.eql? "FileNotFoundPage" or child_page.part("no-map")
+    return if depth.to_i < 0 or child_page.virtual? or !child_page.published? or child_page.class_name.eql? "FileNotFoundPage" or child_page.part("no-map")
     
     r = %{<li#{li_attrs_for_current_page_vs_navigation_item(current_page, child_page)}>
       #{link_for_page(child_page)}\n}
       # mind the open li
     rr = ""
     if child_page.children.size > 0 and 
-        @depth.to_i > 0 and
+        depth.to_i > 0 and
         child_page.class_name != 'ArchivePage' and
         (@expand_all || current_page.url.starts_with?(child_page.url) )
       @first_set = false
       child_page.children.each do |child|
-        rr << tag.render('sub-nav', :page => child, :depth => @depth.to_i - 1, :first_set => @first_set ) unless child.part("no-map") || !child.published?
+        rr << tag.render('sub-nav', :page => child, :depth => depth.to_i - 1, :first_set => @first_set ) unless child.part("no-map") || !child.published?
       end
       
       r << "<ul>\n" + rr + "</ul>\n" unless rr.empty?