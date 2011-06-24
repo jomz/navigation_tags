b06cfcab1358a2f21b2e646065e17b1ccb1291fd
Benny Degezelle <benny@gorilla-webdesign.be>
2011-06-24 02:06:53 +0200

    updating README, bumping version to 2.3

--- navigation_tags_extension.rb
+++ navigation_tags_extension.rb
@@ -1,5 +1,5 @@
 class NavigationTagsExtension < Radiant::Extension
-  version "2.2"
+  version "2.3"
   description "Makes building navigations much easier."
   url "https://github.com/jomz/navigation_tags"
     
